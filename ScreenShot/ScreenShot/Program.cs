4806a7ec295019cd3a11c456d7e02b74b560edff
DrMuttley <lukas216@vp.pl>
2021-04-12 14:43:28 +0200

    ScreenShot
    App make screenshot and save as png file every 3 sec.

---
+++ ScreenShot/ScreenShot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace ScreenShot
+{
+    class Program
+    {
+        private static int counter = 0;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(3000);
+                makeScreenShot();
+            }
+        }
+        static private void makeScreenShot()
+        {
+            Bitmap bmp = new Bitmap(
+                Screen.PrimaryScreen.Bounds.Width, 
+                Screen.PrimaryScreen.Bounds.Height);
+
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+
+            bmp.Save($"{counter}.png");
+            counter++;
+        }
+
+        //wysyłanie maili
+
+        //wczytywanie zmiennych z pliku
+    }
+}
